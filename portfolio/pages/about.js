6506fd0376e24e648f739a37b75f894d45a0108e
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-19 08:42:55 +0900

    fetched github api with isomorphic-unfetch

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -1,12 +1,34 @@
 import Link from 'next/link';
 import Layout from '../components/layout';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-export default () => (
-  <Layout title="about">
-    <Link href="/">
-      <a>Go to homepage</a>
-    </Link>
-    <p>A JS programmer</p>
-    <img src="/static/cat_javascript.png" alt="" />
-  </Layout>
-);
+export default class About extends Component {
+  state = {
+    user: null,
+  };
+  //   componentDidMount() {
+  //     fetch('https://api.github.com/users/ShotaTogawa')
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         this.setState({ user: data });
+  //       });
+  //   }
+
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/ShotaTogawa');
+    const data = await res.json();
+
+    return { user: data };
+  }
+  render() {
+    const { user } = this.props;
+    return (
+      <Layout title="about">
+        <p>{user.name}</p>
+        <p>A JS programmer</p>
+        <img src={user.avatar_url} alt="Me" height="200px" />
+      </Layout>
+    );
+  }
+}
