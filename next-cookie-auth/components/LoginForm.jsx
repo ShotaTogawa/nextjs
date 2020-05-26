48f398b99673e3f87f74951ace9adbbc00788152
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-27 06:37:53 +0900

    implemented simple login form

---
+++ next-cookie-auth/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { loginUser } from '../lib/auth';
+
+class LoginForm extends React.Component {
+  state = {
+    email: 'Sincere@april.biz',
+    password: 'hildegard.org',
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    loginUser(email, password);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
