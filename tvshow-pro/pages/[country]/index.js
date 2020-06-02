9ae7e5522ce9e4d2e2cf21c0c105ce781da96a29
ShotaTogawa <s.togawa0112@gmail.com>
2020-06-02 14:12:19 +0900

    fetinch data on client side

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,3 +1,14 @@
-const CountryTest = () => (<h1>Testing</h1>);
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const CountryTest = () => {
+  useEffect(() => {
+    axios
+      .get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+      .then((response) => console.log(response.data));
+  }, []);
+
+  return <h1>Home</h1>;
+};
 
 export default CountryTest;
