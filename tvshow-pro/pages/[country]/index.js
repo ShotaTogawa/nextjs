580e187ae9051369f066823dcf7bdb0031543cea
ShotaTogawa <s.togawa0112@gmail.com>
2020-06-02 14:19:00 +0900

    fetching data on server side

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,14 +1,15 @@
-import { useEffect } from 'react';
 import axios from 'axios';
 
-const CountryTest = () => {
-  useEffect(() => {
-    axios
-      .get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-      .then((response) => console.log(response.data));
-  }, []);
-
+const CountryTest = ({ shows }) => {
   return <h1>Home</h1>;
 };
 
+CountryTest.getInitialProps = async () => {
+  const response = await axios.get(
+    'http://api.tvmaze.com/schedule?country=US&date=2014-12-01'
+  );
+  return {
+    shows: response.data,
+  };
+};
 export default CountryTest;
