a8e4fa33e22290d83a801b11a990dfdcca541466
ShotaTogawa <s.togawa0112@gmail.com>
2020-06-02 14:54:22 +0900

    get query parameter from context

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
 
-const CountryTest = ({ shows }) => {
-  return <h1>Home</h1>;
+const Home = ({ shows }) => {
+  const renderShows = () => {
+    return shows.map((showItem, index) => {
+      const { show } = showItem;
+      return <li key={index}>{show.name}</li>;
+    });
+  };
+  return <ul className="tvshows">{renderShows()}</ul>;
 };
 
-CountryTest.getInitialProps = async () => {
+// こっちが最初に実行され、returnが返り次第画面描画される
+Home.getInitialProps = async (context) => {
+  console.log(context);
+  const country = context.query.country || 'us';
   const response = await axios.get(
-    'http://api.tvmaze.com/schedule?country=US&date=2014-12-01'
+    `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
   );
   return {
     shows: response.data,
   };
 };
-export default CountryTest;
+export default Home;
